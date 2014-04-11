e7b546b10ffedb6bab85c89ca1dd46c86c4ff80a
raider001 <furnell.daniel@gmail.com>
2014-04-11 20:18:52 +0930

    Lots of changes
    Lots and lots and lots

--- svg2svgbasic/src/com/raider00321/converters/Converter1P0To1P1Basic.java
+++ svg2svgbasic/src/com/raider00321/converters/Converter1P0To1P1Basic.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Converter1P0To1P1Basic 
@@ -42,11 +44,12 @@ public class Converter1P0To1P1Basic
 			String op = output.toString() +"\\"+ file.toString().subSequence(file.toString().indexOf("\\") + 1, file.toString().length());
 			System.out.println(op);
 			PrintWriter writer = new PrintWriter(op);
-			
+			addTabs();
 			RemoveSVGTags();
 			ConvertProperties();
 			ConvertRGB();
 			addComment();
+			addRecursion();
 			for(int k = 0; k < list.size(); k++)
 			{
 				writer.println(list.get(k));
@@ -59,7 +62,66 @@ public class Converter1P0To1P1Basic
 			e.printStackTrace();
 		}
 	}
-	
+	public void addRecursion()
+	{
+		//this code gets the data that may be recursive
+		Map<String,ArrayList> map = new HashMap<String,ArrayList>();
+		ArrayList<String> tempList = new ArrayList<String>();
+		boolean inDefs = false;
+		String mapName = "";
+		for(int i = 0; i < list.size(); i ++)
+		{
+			
+			if(list.get(i).contains("</defs>"))
+			{
+				map.put(mapName, tempList);
+				inDefs = false;
+			}
+			if(inDefs)
+			{
+				if(list.get(i).contains("id="))
+				{
+					mapName = list.get(i).substring(list.get(i).indexOf("id=\"")+4,list.get(i).indexOf("\"",list.get(i).indexOf("id=\"")+4));
+					System.out.println("mapName: "+mapName);
+				}
+				tempList.add(list.get(i));
+			}
+			if(list.get(i).contains("<defs>"))
+			{
+				tempList = new ArrayList<String>();
+				inDefs = true;
+			}
+			
+		}
+		//end of getting the recursive parts. Starts to add the new data
+		for(int i = 0; i < list.size(); i ++)
+		{
+			if(list.get(i).contains("<use"))
+			{
+				int hashInt = list.get(i).indexOf("#");
+				int endOfID = list.get(i).indexOf("\"", hashInt);
+				String id = list.get(i).substring(hashInt+1,endOfID);
+				map.get(id);
+				for(int j = map.get(id).size() - 1; j >= 0; j--)
+				{
+					String currLine = (String)map.get(id).get(j);
+					if(currLine.contains("id=\""+id))
+					{
+						int startProperties = list.get(i).indexOf("\"",list.get(i).indexOf(id)) + 1;
+						String properties = list.get(i).substring(startProperties,list.get(i).lastIndexOf(">"));
+						currLine = currLine.replace(">", properties + ">");
+						System.out.println(currLine);
+					}
+					list.add(i + 1,currLine);
+				}
+				list.remove(i);
+			}
+			if(list.get(i).contains("</use>"))
+			{
+				list.remove(i);
+			}
+		}
+	}
 	/*
 	 * Modify property values
 	 */
@@ -88,6 +150,9 @@ public class Converter1P0To1P1Basic
 			
 		}
 	}
+	 /* 
+	  * Modify property values
+	  */
 	public void ConvertProperties()
 	{
 		for(int i = 0; i < list.size();i++)
@@ -95,56 +160,80 @@ public class Converter1P0To1P1Basic
 			if(list.get(i).contains("style="))
 			{
 				list.set(i,list.get(i).replace("style=\"",""));
-				list.get(i).lastIndexOf("\"");
 				list.set(i, list.get(i).substring(0, list.get(i).lastIndexOf("\"")) + list.get(i).substring(list.get(i).lastIndexOf("\"")+1));
 				list.set(i, list.get(i).replaceAll(":","=\""));
 				list.set(i, list.get(i).replaceAll(";","\" "));
-				list.set(i, list.get(i).replace(">", "\" >"));
+				//list.set(i, list.get(i).substring(0, list.get(i).lastIndexOf("\"")) + list.get(i).substring(list.get(i).lastIndexOf("\"")+1));
+				String temp = ""; 
+				temp = list.get(i).substring(0,list.get(i).indexOf(">",list.get(i).indexOf("="))) + "\"";
+				
+				list.get(i).indexOf(">",list.get(i).lastIndexOf("="))
+				
+				temp = temp + list.get(i).substring(list.get(i).indexOf(">",list.get(i).lastIndexOf("=")),list.get(i).length());
+				list.set(i, temp);
+				list.set(i, list.get(i).replace(" \">", ">"));
 			}
 		}
 	}
 	/*
-	 * Removes all viewBox svg tags from file
+	 * Makes it more readable and parseable
 	 */
-	
+	public void addTabs()
+	{
+		for(int i = 0; i < list.size(); i++)
+		{
+			String tempList[];
+			list.get(i).replaceAll(">", ">lL");
+			tempList = list.get(i).split("lL");
+			System.out.println(list.get(i));
+			if(tempList.length > 1)
+			{
+				
+				for(int j = tempList.length - 1; j >= 0; j--)
+				{
+					list.add(i+1,tempList[j] + ">");
+				}
+				list.remove(i);
+
+			}
+		
+		}
+	}
 	public void addComment()
 	{
 		list.add("<!-- Converted By SVG2SVGBasic Converter 2014 By Raider00321 -->");
 	}
+	/*
+	 * removes all viewbox svg tags
+	 */
 	public void RemoveSVGTags()
 	{
 		int svgDepth = 0;
 		for(int i=0;i<list.size();i++) 
 		{
-			if(list.get(i).indexOf("<svg") != -1)
+			if(list.get(i).indexOf("<svg viewBox") != -1)
+			{
+				
+				list.remove(i);
+				System.out.println(list.get(i));
+
+			}
+			if(list.get(i).contains("<svg"))
 			{
 				svgDepth +=1;
 			}
-			if(list.get(i).indexOf("</svg>") != -1)
+		}
+		int currentDepth = 0;
+		for(int i = list.size()-1; i >= 0; i--)
+		{
+			if(list.get(i).contains("</svg>"))
 			{
-				svgDepth -=1;
+				currentDepth++;
 			}
-			if(list.get(i).indexOf("<svg viewBox") != -1)
+			if(currentDepth > svgDepth)
 			{
-				int removalStart = list.get(i).indexOf("<svg viewBox");
-				int removalEnd = list.get(i).indexOf(">", removalStart);
-				list.set(i,list.get(i).substring(removalEnd + 1));
-				int tempRef = 0;
-				for(int j = list.size()-1; j > 0; j--)
-				{
-					if(list.get(j).indexOf("</svg>") != -1)
-					{
-						
-						tempRef++;
-						if(tempRef == svgDepth)
-						{
-							System.out.println("bang");
-							svgDepth--;
-							list.set(j, list.get(j).replace("</svg>", ""));
-
-						}
-					}
-				}
+				currentDepth--;
+				list.remove(i);
 			}
 		}
 		
---
+++ svg2svgbasic/src/com/raider00321/converters/ExpandedString.java
@@ -0,0 +1,10 @@
+package com.raider00321.converters;
+
+public class ExpandedString {
+
+	public int firstIndexBefore(String str)
+	{
+		return 0;
+		
+	}
+}
